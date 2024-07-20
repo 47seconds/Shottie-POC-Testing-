328e6f83b0c736ea072d78dfd144ce19d133ca22
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-21 01:29:34 +0530

    added sample shottie stream

--- README.md
+++ README.md
@@ -1,13 +1,4 @@
 # shottie-[poc-&-testing]
 
-To install dependencies:
-
-```bash
-bun install
-```
-
-To run:
-
-```bash
-bun start
-```
+### A simple proof of concept test project to integrate shottie features in Gacha
+#### NOTE: Enviroment values are hard coded. A hls m3u8 index file is provided for testing purposes.
--- server.js
+++ server.js
@@ -3,6 +3,10 @@ import cors from 'cors';
 import multer from 'multer';
 import { nanoid } from 'nanoid';
 import path from "path";
+import fs from 'fs';
+import { exec } from 'child_process';
+import { error } from 'console';
+import { stderr, stdout } from 'process';
 
 const app = express();
 
@@ -40,8 +44,37 @@ app.get('/', (req, res) => {
 
 // MAIN
 app.use('/upload', upload.single("shottie"), (req, res) => {
-    console.log("shottie uploaded!");
-    res.json({message: "shottie uploaded successfully"});
+    
+    const shottieId = nanoid(10);
+    const shottiePath = req.file.path;
+    const outputPath = `./uploads/shotties/${shottieId}`;
+    const hlsPath = `${outputPath}/index.m3u8`;     // making a index file for our video
+
+    if (!fs.existsSync(outputPath)) {
+        fs.mkdirSync(outputPath, {recursive: true});    // our directory have multiple and sub-dierectories, therefore recursice = true
+    }
+
+    // ffmpeg
+    const ffmpegCommand = `ffmpeg -i ${shottiePath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
+    
+    // NOT to be run on server, but on seperate instances with queues
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`exec errors: ${error}`);
+            fs.unlinkSync(shottiePath);
+        };
+        console.log(`stdout: ${stdout}`);       
+        console.log(`stderr: ${stderr}`);
+        
+        fs.unlinkSync(shottiePath);
+
+        const shottieUrl = `http://localhost:3000/uploads/shotties/${shottieId}/index.m3u8`;
+
+        res.json({
+            message: "Video converted successfully",
+            videoUrl: shottieUrl,
+        });
+    });
 });
 
 
