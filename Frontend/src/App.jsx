2a92ba8477f64d996bbed82244152eb8c1186810
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-21 01:28:36 +0530

    made frontend for accessing stream

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,39 @@
+import { useRef } from 'react';
+import VideoPlayer from './components/VideoPlayer';
+
 function App() {
+  const videoPlayer = useRef(null);
+  const shottieLink = "http://localhost:3000/uploads/shotties/StWGbEknxy/index.m3u8";
+
+  const VideoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: shottieLink,
+        type: "application/x-mpegURL"
+      }
+    ]
+  }
+
+  const handlePlayerReady = (player) => {
+    videoPlayer.current = player;
+
+    // handling player
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  }
+
   return (
     <>
-      
+      <h1>Shottie</h1>
+      <VideoPlayer options={VideoPlayerOptions} onReady={handlePlayerReady}/>
     </>
   )
 }
